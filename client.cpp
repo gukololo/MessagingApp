ab9a4b57df67437b235de6b66d90c5ca382a77c3
Gürkan Çakmak <gurkancakmakk@hotmail.com>
2025-06-18 17:44:50 +0300

    portNumber update is added

--- client.cpp
+++ client.cpp
@@ -30,11 +30,12 @@ int main() {
     inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
     connect(sock, (sockaddr*)&server_addr, sizeof(server_addr));
 
+    int server_size = sizeof(server_addr);
+    SOCKET server_socket = accept(sock, (sockaddr*)&server_addr, &server_size);
+
 
     //then checking if it is available to connect
     char buffer[1024];
-    int server_size = sizeof(server_addr);
-    SOCKET server_socket = accept(sock, (sockaddr*)&server_addr, &server_size);
     memset(buffer, 0, sizeof(buffer));
     recv(sock, buffer, sizeof(buffer), 0);
 
@@ -79,12 +80,70 @@ int main() {
         cout <<"received duplicated answer: " << duplicatedAnswer << endl;
     }
         if (duplicatedAnswer == "okey") {
-            cout <<"You are registered succesfully!"<< endl;
+            cout <<"You are registered successfully!"<< endl;
+        }
+
+        //registration is successful
+        cout << "Connected to server!" << endl;
+
+
+
+        //after registration, the new port number is received and the communication is changing according to the new port number
+        int newPortNumber;
+        recv(sock, (char*)&newPortNumber, sizeof(newPortNumber), 0);
+        cout << "NEW PORT NUM: "<<newPortNumber << endl;
+
+        closesocket(sock);
+        closesocket(server_socket);
+
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+
+        sockaddr_in newServer_addr{};
+        newServer_addr.sin_family = AF_INET;
+        newServer_addr.sin_port = htons(newPortNumber);
+        inet_pton(AF_INET, "127.0.0.1", &newServer_addr.sin_addr);
+        connect(sock, (sockaddr*)&newServer_addr, sizeof(newServer_addr));
+
+        server_size = sizeof(newServer_addr);
+        server_socket = accept(sock, (sockaddr*)&newServer_addr, &server_size);
+
+
+        //trying to communciate with server with new port
+        string tempMessage ="kanka port degisti haberin ola";
+        send(sock, tempMessage.c_str(), tempMessage.length(), 0);
+        cout << tempMessage << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //menu opens up and the application starts
+        string action;
+        cout<< "1.Open messaging mode\n2.Choose users to send message\n3.Check for messages\n4.See all available users\n5.See my message history\n6.Disconnect\nWhat do you want to do? "<<endl;
+    cin >> action;
+        //if the input is invalid
+        while (action != "1" && action != "2" && action != "3" && action != "4" ) {
+            cout << "Your choice is invalid. Please enter a valid action (1-2-3-4)!" << endl;
+            cout<< "1.Open messaging mode\n2.Choose users to send message\n3.Check for messages\n4.See all available users\n5.See my message history\n6.Disconnect\nWhat do you want to do? "<<endl;
+            cin >> action;
         }
 
 
-
-    cout << "Connected to server!" << endl;
 
     thread(receive_messages, sock).detach();
 
--- server.cpp
+++ server.cpp
@@ -9,11 +9,13 @@
 #include "ClientUser.cpp"
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
+
 vector<SOCKET> clients;
 mutex clients_mutex;
 // Client storage
 vector<ClientUser> AllClients;
 int client_count = 0;
+int portNumbers[3] = {8081, 8082, 8083};
 
 /**
  * this method checks if the received name is duplicated
@@ -28,6 +30,14 @@ bool isDuplicated(const string &s) {
         }
     }
     return duplicated;
+}
+
+void sendClientAllUsersNames(SOCKET client) {
+    string usernames ="";
+    for (int i = 0; i < AllClients.size(); i++) {
+        usernames += AllClients[i].getClientName() + " ";
+    }
+    send(client,usernames.c_str(), usernames.length(), 0);
 }
 
 void handle_client(SOCKET client_socket) {
@@ -76,7 +86,6 @@ int main() {
 
         lock_guard<mutex> lock(clients_mutex);
 
-
         //first it will send clients the number of clients to decide whether they can continue
         send(client_socket, to_string(AllClients.size()).c_str(), to_string(AllClients.size()).length(), 0);
 
@@ -100,6 +109,7 @@ int main() {
 
         //if it is duplicated, the server will send client the output
         while (duplicated){
+
             duplicatedAnswer ="duplicated";
             //sending the duplicated answer
             send(client_socket, duplicatedAnswer.c_str(), duplicatedAnswer.length(), 0);
@@ -108,13 +118,58 @@ int main() {
             recv(client_socket, buffer, sizeof(buffer), 0);
             receivedName = buffer;
             duplicated = isDuplicated(receivedName);
+
         }
         duplicatedAnswer = "okey";
         send(client_socket, duplicatedAnswer.c_str(), duplicatedAnswer.length(), 0);
+
+        //registering the new client
         ClientUser newClient;
         newClient.setClientName(receivedName);
         newClient.setIsActive(true);
+        client_count++;
+        newClient.setPortNumber(8080 + client_count);
         AllClients.push_back(newClient);
+
+        //register part ended, now the application starts
+        //first the user should receive the special port number
+        //changing the communication according to new port number
+        int portNumberToSend = 8080 + client_count;
+        send(client_socket,(char*)&portNumberToSend,sizeof(portNumberToSend),0);
+        closesocket(server_socket);
+
+        server_socket = socket(AF_INET, SOCK_STREAM, 0);
+        sockaddr_in newServer_addr{};
+        newServer_addr.sin_family = AF_INET;
+        newServer_addr.sin_port = htons(portNumberToSend);
+        newServer_addr.sin_addr.s_addr = INADDR_ANY;
+
+        bind(server_socket, (sockaddr*)&newServer_addr, sizeof(newServer_addr));
+        listen(server_socket, SOMAXCONN);
+
+        closesocket(client_socket);
+        client_size = sizeof(client_addr);
+        client_socket = accept(server_socket, (sockaddr*)&client_addr, &client_size);
+
+        //trying to receive a new message after changing port
+        string tempMessage;
+        memset(buffer, 0, sizeof(buffer));
+        recv(client_socket, buffer, sizeof(buffer), 0);
+        tempMessage = buffer;
+        cout << "tempMessage : " << tempMessage<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         clients.push_back(client_socket);
         thread(handle_client, client_socket).detach();
